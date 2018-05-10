b590d7e43989662a098148cdbe8af4953ad270e8
dbaldwin <baldwin.derrick@gmail.com>
2018-05-10 19:13:51 -0400

    cleanup

--- rust/problem_13/target/debug/libproblem_13.d
+++
@@ -1,1 +0,0 @@
-/home/dbaldwin/euler-project/rust/problem_13/target/debug/libproblem_13.rlib: /home/dbaldwin/euler-project/rust/problem_13/src/lib.rs
--- rust/problem_13/target/debug/problem_13.d
+++
@@ -1,1 +0,0 @@
-/home/dbaldwin/euler-project/rust/problem_13/target/debug/problem_13: /home/dbaldwin/euler-project/rust/problem_13/src/main.rs /home/dbaldwin/euler-project/rust/problem_13/src/lib.rs
